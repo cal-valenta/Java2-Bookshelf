4bfa96c066e4c7e3b93c1afede5cf9afb0378907
Logan Cristopher Baccam <k0832049@kirkwood.edu>
2021-11-04 20:46:35 -0500

    Adding ChildrensBook class

--- src/com/valenta/Main.java
+++ src/com/valenta/Main.java
@@ -3,6 +3,17 @@ package com.valenta;
 public class Main {
 
     public static void main(String[] args) {
+
+
+
+
+
+        Book b4 = new ChildrensBook("Java for Kids", "Logan", "Calvin");
+
+
         System.out.println("Hi");
+
+
+
     }
 }
