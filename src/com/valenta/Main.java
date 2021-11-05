22032c908d123f00a1c9107e8950da9a55b28ca9
Calvin Valenta <k0553822@student.kirkwood.edu>
2021-11-04 20:46:24 -0500

    Add Book class

--- src/com/valenta/Main.java
+++ src/com/valenta/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi");
+        Book b1 = new Book("Java Generics", "Cal");
+        Book b2 = new Book("Java Collections", "Cal");
+        Book b3 = new Book("Java Collections", "Logan");
     }
 }
+
